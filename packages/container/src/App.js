0d3ed2734d1eea2c49b381cd93653e4f7668ba7e
Marcelo Robles <marcelo.robles@sag.gob.cl>
2024-03-14 17:19:57 -0300

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+// import { mount } from 'marketing/MarketingApp';
+// console.log(mount);
+
+export default ()=>{
+    return <div>
+        <h1>hi there!!</h1>
+        
+        <hr/>
+
+        <MarketingApp/>
+
+    </div>
+};
